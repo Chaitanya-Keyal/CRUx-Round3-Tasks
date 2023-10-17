d92f7d72aad80351bd34c333508b983dabdfece1
Chaitanya-Keyal <66475772+Chaitanya-Keyal@users.noreply.github.com>
2023-10-18 04:07:21 +0530

    feat(Spotify2YouTube): add auth for spotify
    - Adds authentication for spotify
    - Initial user details api call

---
+++ Task 2 - Spotify2YouTube/auth.py
@@ -0,0 +1,123 @@
+import json
+import logging
+import secrets
+
+import requests
+from flask import Flask, cli, redirect, request
+
+app = Flask(__name__)
+
+# Disable Flask banner and Werkzeug logger
+cli.show_server_banner = lambda *args: None
+log = logging.getLogger("werkzeug")
+log.setLevel(logging.ERROR)
+
+
+# Spotify app credentials
+f = open("spotify/credentials.json", "r")
+spotify_credentials = json.load(f)
+client_id = spotify_credentials["client_id"]
+client_secret = spotify_credentials["client_secret"]
+redirect_uri = spotify_credentials["redirect_uri"]
+scope = spotify_credentials["scope"].replace(" ", "%20")
+f.close()
+
+BASE_API_URL = "https://api.spotify.com"
+APP_URL, SERVER_PORT = "http://localhost", 8888
+
+state = secrets.token_hex(16)
+
+
+@app.route("/auth", methods=["GET"])
+def get_auth_code():
+    """
+    Redirect user to Spotify login page.
+
+    Args:
+        None
+    Returns:
+        auth_code (str): Authorization code
+    """
+    return redirect(
+        "https://accounts.spotify.com/authorize?"
+        + f"client_id={client_id}"
+        + "&response_type=code"
+        + f"&redirect_uri={redirect_uri}"
+        + f"&scope={scope}"
+        + f"&state={state}"
+        + "&show_dialog=true"
+    )
+
+
+@app.route("/callback", methods=["GET"])
+def callback():
+    """
+    Stores authorization code in JSON file.
+    """
+    recv_state = request.args.get("state")
+    if not recv_state:
+        raise Exception("No state received.")
+    else:
+        if recv_state != state:
+            raise Exception("State mismatch.")
+    try:
+        auth_code = request.args.get("code")
+        r = requests.post(
+            "https://accounts.spotify.com/api/token",
+            data={
+                "grant_type": "authorization_code",
+                "code": auth_code,
+                "redirect_uri": redirect_uri,
+                "client_id": client_id,
+                "client_secret": client_secret,
+            },
+        )
+        if r.status_code != 200:
+            raise Exception(f"Status code: {r.status_code}, {r.text}")
+        token = r.json()
+        with open("spotify/token.json", "w") as f:
+            json.dump(token, f)
+        return "Authorisation Successful. You can close this tab and return to the application."
+    except ValueError:
+        error = request.args.get("error")
+        raise Exception(f"Error: {error}")
+
+
+def get_access_token():
+    """
+    Gets access token from JSON file.
+    If access token is invalid or expired, refreshes access token.
+
+    Args:
+        None
+    Returns:
+        access_token (str): Access token
+    """
+    token = {}
+    with open("spotify/token.json", "r+") as f:
+        token = json.load(f)
+        r = requests.get(
+            f"{BASE_API_URL}/v1/me",
+            headers={"Authorization": f"Bearer {token['access_token']}"},
+        )
+        if r.status_code != 200:
+            r = requests.post(
+                "https://accounts.spotify.com/api/token",
+                data={
+                    "grant_type": "refresh_token",
+                    "refresh_token": token["refresh_token"],
+                    "client_id": client_id,
+                    "client_secret": client_secret,
+                },
+            )
+            if r.status_code != 200:
+                raise Exception(f"Status code: {r.status_code} {r.text}")
+            token.update(r.json())
+            f.seek(0)
+            f.truncate()
+            json.dump(token, f)
+    return token["access_token"]
+
+
+if __name__ == "__main__":
+    app.run(host=APP_URL.removeprefix("http://"), port=SERVER_PORT)
---
+++ Task 2 - Spotify2YouTube/spotify.py
@@ -0,0 +1,62 @@
+import json
+import os
+import threading
+
+import requests
+from auth import APP_URL, BASE_API_URL, SERVER_PORT, app, get_access_token
+
+
+def pretty_print(json_object):
+    print(json.dumps(json_object, indent=4))
+
+
+def get_user_details(access_token):
+    """
+    Get user details from Spotify.
+
+    Args:
+        access_token (str): Access token
+    Returns:
+        user (dict): User details
+    """
+    r = requests.get(
+        f"{BASE_API_URL}/v1/me",
+        headers={"Authorization": f"Bearer {access_token}"},
+    )
+    if r.status_code != 200:
+        raise Exception(f"Status code: {r.status_code} {r.text}")
+    user = r.json()
+    return user
+
+
+def authorise():
+    """
+    Gets the user to authorize this app.
+    """
+    print(
+        f"Click the following link to authorize this app:\n{APP_URL}:{SERVER_PORT}/auth"
+    )
+
+    thread = threading.Thread(
+        target=app.run,
+        kwargs={"host": APP_URL.removeprefix("http://"), "port": SERVER_PORT},
+        daemon=True,
+    )
+    thread.start()
+
+    # Waiting for user to authorize app
+    while not os.path.exists("spotify/token.json"):
+        pass
+
+
+def main():
+    if not os.path.exists("spotify/token.json"):
+        authorise()
+
+    user = get_user_details(get_access_token())
+    print(f"Welcome, {user['display_name']}!")
+    pretty_print(user)
+
+
+if __name__ == "__main__":
+    main()
